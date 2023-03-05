5387ed1f8d48d84cce65dfcf8e6d1f960c3752a7
namle <namlgcd19254@fpt.edu.vn>
2023-03-05 20:46:34 +0700

    Setup modelview, extension

---
+++ IDEA_Collection/ModelViews/ChangePasswordViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IDEA_Collection.ModelViews
+{
+    public class ChangePasswordViewModel
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Vui lòng email đăng nhập")]
+        public string Email { get; set; }
+        [Display(Name = "Avata")]
+        public string Avata { get; set; }
+
+        [Display(Name = "Mật khẩu hiện tại")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu hiện tại")]
+        public string PasswordNow { get; set; }
+
+        [Display(Name = "Mật khẩu mới")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới")]
+        [MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
+        public string Password { get; set; }
+
+        [MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
+        [Display(Name = "Nhập lại mật khẩu")]
+        [Compare("Password", ErrorMessage = "Nhập lại mật khẩu không đúng")]
+        public string ConfirmPassword { get; set; }
+    }
+}
