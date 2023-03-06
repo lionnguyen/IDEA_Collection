0b23b45d8c795310d9ed92f9faea6192e7419eba
namle <namlgcd19254@fpt.edu.vn>
2023-03-06 15:04:14 +0700

    Set conditions for admin home page and qa home page

---
+++ IDEA_Collection/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using IDEA_Collection.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Text;
+namespace IDEA_Collection.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public INotyfService _notyfService { get; }
+        private readonly CollectIdeasContext _context;
+
+        public HomeController(INotyfService notyfService, CollectIdeasContext context)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+        [Area("Admin")]
+        [Route("admin", Name = "AdminIndex")]
+        [Authorize]
+        public IActionResult Index()
+        {
+
+            var taikhoanID = HttpContext.Session.GetString("AccountId");
+            var roleID = int.Parse(HttpContext.Session.GetString("RoletId"));
+            if (taikhoanID == null || roleID != 1)
+            {
+                _notyfService.Success("You cannot access this page!");
+                return RedirectToAction("Index", "Home", new { Area = "" });
+            }
+            if (taikhoanID != null)
+            {
+                var account = _context.Accounts.AsNoTracking().Include(x => x.Department).SingleOrDefault(x => x.AccountId == Convert.ToInt32(taikhoanID));
+
+                if (account != null)
+                {
+                    var avata = account.Avatar;
+                    ViewBag.avata = avata;
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ IDEA_Collection/Areas/QADepartment/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using IDEA_Collection.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace IDEA_Collection.Areas.QADepartment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public INotyfService _notyfService { get; }
+        private readonly CollectIdeasContext _context;
+        public HomeController(INotyfService notyfService, CollectIdeasContext context)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+
+        [Authorize]
+        [Area("QADepartment")]
+        [Route("qa.html", Name = "QA")]
+        public IActionResult Index()
+        {
+            var taikhoanID = HttpContext.Session.GetString("AccountId");
+            var roleID = int.Parse(HttpContext.Session.GetString("RoletId"));
+            if (taikhoanID == null || roleID != 1002)
+            {
+                _notyfService.Success("You cannot access this page!");
+                return RedirectToAction("Index", "Home", new { Area = "" });
+            }
+            if (taikhoanID != null)
+            {
+                var khachhang = _context.Accounts.AsNoTracking().Include(x => x.Department).SingleOrDefault(x => x.AccountId == Convert.ToInt32(taikhoanID));
+
+                if (khachhang != null)
+                {
+                    var avata = khachhang.Avatar;
+                    var department = khachhang.Department.DepartmentName;
+                    ViewBag.avata = avata;
+                    ViewBag.department = department;
+                }
+            }
+            return View();
+        }
+    }
+}
