ff4895748998174cefeb415888027058ea2d43e0
namle <namlgcd19254@fpt.edu.vn>
2023-03-06 14:01:31 +0700

    IDEA-51 Send mail

---
+++ IDEA_Collection/Email/ISendMailService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace IDEA_Collection.Email
+{
+    public interface ISendMailService
+    {
+        Task SendMail(MailContent mailContent);
+
+        Task SendEmailAsync(string email, string subject, string htmlMessage);
+    }
+}
---
+++ IDEA_Collection/Email/MailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace IDEA_Collection.Email
+{
+    public class MailSettings
+    {
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+    }
+}
