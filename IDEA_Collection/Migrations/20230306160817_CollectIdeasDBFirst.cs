90de50956be6ef02201b28e71bc19439f113b5e2
namle <namlgcd19254@fpt.edu.vn>
2023-03-07 10:50:42 +0700

    add migration

---
+++ IDEA_Collection/Migrations/20230306160817_CollectIdeasDBFirst.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IDEA_Collection.Migrations
+{
+    public partial class CollectIdeasDBFirst : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CatID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CatName = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Published = table.Column<bool>(type: "bit", nullable: false),
+                    Thumb = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CatID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    StartDates = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ClosureDates = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RoleID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RoleID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    AccountID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Phone = table.Column<string>(type: "varchar(12)", unicode: false, maxLength: 12, nullable: true),
+                    Birthday = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    RoleID = table.Column<int>(type: "int", nullable: true),
+                    DepartmentID = table.Column<int>(type: "int", nullable: true),
+                    LastLogin = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Avatar = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountID);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Departments",
+                        column: x => x.DepartmentID,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Roles",
+                        column: x => x.RoleID,
+                        principalTable: "Roles",
+                        principalColumn: "RoleID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ideas",
+                columns: table => new
+                {
+                    PostID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    SContents = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Contents = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Thumb = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Published = table.Column<bool>(type: "bit", nullable: false),
+                    Anonymously = table.Column<bool>(type: "bit", nullable: false),
+                    Alias = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    AccountID = table.Column<int>(type: "int", nullable: true),
+                    CatID = table.Column<int>(type: "int", nullable: true),
+                    Likes = table.Column<int>(type: "int", nullable: true),
+                    Unlikes = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ideas", x => x.PostID);
+                    table.ForeignKey(
+                        name: "FK_Ideas_Accounts",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "AccountID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Ideas_Categories",
+                        column: x => x.CatID,
+                        principalTable: "Categories",
+                        principalColumn: "CatID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Contents = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Thumb = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Published = table.Column<bool>(type: "bit", nullable: false),
+                    Alias = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    AccountID = table.Column<int>(type: "int", nullable: true),
+                    Likes = table.Column<int>(type: "int", nullable: true),
+                    Unlikes = table.Column<int>(type: "int", nullable: true),
+                    Anonymously = table.Column<bool>(type: "bit", nullable: false),
+                    PostID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentID);
+                    table.ForeignKey(
+                        name: "FK_Comments_Accounts",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "AccountID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comments_Ideas",
+                        column: x => x.PostID,
+                        principalTable: "Ideas",
+                        principalColumn: "PostID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    LikeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostID = table.Column<int>(type: "int", nullable: true),
+                    AccountID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => x.LikeID);
+                    table.ForeignKey(
+                        name: "FK_Likes_Accounts",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "AccountID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Likes_Ideas",
+                        column: x => x.PostID,
+                        principalTable: "Ideas",
+                        principalColumn: "PostID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Unlikes",
+                columns: table => new
+                {
+                    UnlikeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostID = table.Column<int>(type: "int", nullable: true),
+                    AccountID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Unlikes", x => x.UnlikeID);
+                    table.ForeignKey(
+                        name: "FK_Unlikes_Accounts",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "AccountID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Unlikes_Ideas",
+                        column: x => x.PostID,
+                        principalTable: "Ideas",
+                        principalColumn: "PostID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_DepartmentID",
+                table: "Accounts",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_RoleID",
+                table: "Accounts",
+                column: "RoleID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AccountID",
+                table: "Comments",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostID",
+                table: "Comments",
+                column: "PostID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ideas_AccountID",
+                table: "Ideas",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ideas_CatID",
+                table: "Ideas",
+                column: "CatID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_AccountID",
+                table: "Likes",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_PostID",
+                table: "Likes",
+                column: "PostID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Unlikes_AccountID",
+                table: "Unlikes",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Unlikes_PostID",
+                table: "Unlikes",
+                column: "PostID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Likes");
+
+            migrationBuilder.DropTable(
+                name: "Unlikes");
+
+            migrationBuilder.DropTable(
+                name: "Ideas");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
